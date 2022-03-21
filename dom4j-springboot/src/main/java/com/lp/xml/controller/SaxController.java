c07dd20f35f9b194f61c06be40d437d7ee7da829
lp-mac <122792286@qq.com>
2022-03-21 16:17:29 +0800

    SAXParserFactory.newInstance()

--- dom4j-springboot/src/main/java/com/lp/xml/controller/SaxController.java
+++ dom4j-springboot/src/main/java/com/lp/xml/controller/SaxController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
+import java.io.StringReader;
 import java.util.List;
 @RestController
 public class SaxController {
@@ -15,6 +16,16 @@ public class SaxController {
     public List<Book> getBooks(String path){
         List<Book> books = XMLParserSAX.xmlReader(new File(path));
 //		List<Book> books = saxParser(new File("books.xml"));
+        for (Book book : books) {
+            System.out.println(book.toString());
+        }
+        return books;
+    }
+
+    //http://localhost:8080/4j/getBooks2?xml=%3C?xml%20version=%221.0%22%20encoding=%22UTF-8%22?%3E%20%3C!--%20Edited%20by%20XMLSpy%20--%3E%20%3Cbookstore%3E%20%3Cbook%20category=%22cooking%22%3E%20%3Ctitle%20lang=%22en%22%3EEveryday%20Italian%3C/title%3E%20%3Cauthor%3EGiada%20De%20Laurentiis%3C/author%3E%20%3Cyear%3E2005%3C/year%3E%20%3Cprice%3E30.00%3C/price%3E%20%3C/book%3E%20%3Cbook%20category=%22children%22%3E%20%3Ctitle%20lang=%22en%22%3EHarry%20Potter%3C/title%3E%20%3Cauthor%3EJ%20K.%20Rowling%3C/author%3E%20%3Cyear%3E2005%3C/year%3E%20%3Cprice%3E29.99%3C/price%3E%20%3C/book%3E%20%3Cbook%20category=%22web%22%3E%20%3Ctitle%20lang=%22en%22%3EXQuery%20Kick%20Start%3C/title%3E%20%3Cauthor%3EJames%20McGovern%3C/author%3E%20%3Cauthor%3EPer%20Bothner%3C/author%3E%20%3Cauthor%3EKurt%20Cagle%3C/author%3E%20%3Cauthor%3EJames%20Linn%3C/author%3E%20%3Cauthor%3EVaidyanathan%20Nagarajan%3C/author%3E%20%3Cyear%3E2003%3C/year%3E%20%3Cprice%3E49.99%3C/price%3E%20%3C/book%3E%20%3Cbook%20category=%22web%22%20cover=%22paperback%22%3E%20%3Ctitle%20lang=%22en%22%3ELearning%20XML%3C/title%3E%20%3Cauthor%3EErik%20T.%20Ray%3C/author%3E%20%3Cyear%3E2003%3C/year%3E%20%3Cprice%3E39.95%3C/price%3E%20%3C/book%3E%20%3C/bookstore%3E
+    @GetMapping("getBooks2")
+    public List<Book> getBooks2(String xml){
+        List<Book> books = XMLParserSAX.xmlReader2(xml);
         for (Book book : books) {
             System.out.println(book.toString());
         }
--- dom4j-springboot/src/main/java/com/lp/xml/sax/XMLParserSAX.java
+++ dom4j-springboot/src/main/java/com/lp/xml/sax/XMLParserSAX.java
@@ -7,6 +7,7 @@ import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.StringReader;
 import java.util.List;
 
 /**
@@ -19,7 +20,40 @@ public class XMLParserSAX {
  
 	public static void main(String[] args) {
 		File file = new File("books.xml");
-		List<Book> books = xmlReader(file);
+//		List<Book> books = xmlReader(file);
+		String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+				"<!-- Edited by XMLSpy -->\n" +
+				"<bookstore>\n" +
+				"    <book category=\"cooking\">\n" +
+				"        <title lang=\"en\">Everyday Italian</title>\n" +
+				"        <author>Giada De Laurentiis</author>\n" +
+				"        <year>2005</year>\n" +
+				"        <price>30.00</price>\n" +
+				"    </book>\n" +
+				"    <book category=\"children\">\n" +
+				"        <title lang=\"en\">Harry Potter</title>\n" +
+				"        <author>J K. Rowling</author>\n" +
+				"        <year>2005</year>\n" +
+				"        <price>29.99</price>\n" +
+				"    </book>\n" +
+				"    <book category=\"web\">\n" +
+				"        <title lang=\"en\">XQuery Kick Start</title>\n" +
+				"        <author>James McGovern</author>\n" +
+				"        <author>Per Bothner</author>\n" +
+				"        <author>Kurt Cagle</author>\n" +
+				"        <author>James Linn</author>\n" +
+				"        <author>Vaidyanathan Nagarajan</author>\n" +
+				"        <year>2003</year>\n" +
+				"        <price>49.99</price>\n" +
+				"    </book>\n" +
+				"    <book category=\"web\" cover=\"paperback\">\n" +
+				"        <title lang=\"en\">Learning XML</title>\n" +
+				"        <author>Erik T. Ray</author>\n" +
+				"        <year>2003</year>\n" +
+				"        <price>39.95</price>\n" +
+				"    </book>\n" +
+				"</bookstore>";
+		List<Book> books = xmlReader2(xml);
 //		List<Book> books = saxParser(new File("books.xml"));
 		for (Book book : books) {
 			System.out.println(book.toString());
@@ -66,5 +100,36 @@ public class XMLParserSAX {
 		}
 		return null;
 	}
- 
+
+	//使用XMLReader 来解析
+	public static List<Book> xmlReader2(String xml) {
+		try {
+			// 1.新建一个工厂类SAXParserFactory
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+			// 2.让工厂类产生一个SAX的解析类SAXParser
+			SAXParser parser = factory.newSAXParser();
+			// 3.从SAXPsrser中得到一个XMLReader实例
+			XMLReader reader = parser.getXMLReader();
+			reader.setFeature("http://xml.org/sax/features/namespaces", true);
+			reader.setFeature("http://xml.org/sax/features/namespace-prefixes", false);
+			reader.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+			reader.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+			reader.setFeature("http://xml.org/sax/features/external-general-entities", false);
+			reader.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+			reader.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+			// 4.得到内容处理器
+			SaxHandler saxHandler = new SaxHandler();
+			// 5.把自己写的handler注册到XMLReader中，一般最重要的就是ContentHandler
+			reader.setContentHandler(saxHandler);
+			// 6.将一个xml文档或者资源变成一个java可以处理的InputStream流后，解析正式开始
+			StringReader stringReader = new StringReader(xml);
+			InputSource inputSource = new InputSource(stringReader);
+			reader.parse(inputSource);
+			List<Book> books = saxHandler.getBookList();
+			return books;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
